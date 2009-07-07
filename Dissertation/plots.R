11fa2fe0e1f48c52a6de6331eea3b3cff593ff98
nick.shrine <nick.shrine@4232b74a-9ac4-11dd-a56d-472d35d90729>
2009-07-07 15:24:44 +0000

    fixed stat_summary with lines

--- Dissertation/plots.R
+++ Dissertation/plots.R
@@ -113,9 +113,9 @@ predoseaov <- function(data, title="") {
 }
 
 allaov <- function(data, title="") {
-	q <- qplot(pt, parct, data=data, xlab="", ylab="Parasite Count (1000s)", main=title, geom="blank")
+	q <- qplot(pt, log((1 + parct)/pre), data=data, xlab="Planned time from first dose", ylab="Log multiple of pre-dose count", main=title, geom="blank")
 	q <- q + geom_point(aes(colour=trttxt), position="dodge") + scale_colour_discrete("Treatment")
-	q <- q + scale_y_continuous(formatter=function(x) return(x/1000)) #+ opts(axis.text.x = theme_text(angle=90))
-	q <- q + stat_summary(fun.y="mean", geom="point", colour="black", size=3)
-	q + facet_grid(SEX~CENTREID, scales="free") 
+#	q <- q + scale_y_continuous(formatter=function(x) return(x/1000)) #+ opts(axis.text.x = theme_text(angle=90))
+	q <- q + stat_summary(fun.y="mean", geom="line", aes(colour=trttxt))
+	q + facet_grid(SEX~CENTREID, margins=T)
 }
