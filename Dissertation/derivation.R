c51ed46507c64ea896ada095893f66e22c9c66f7
nick.shrine <nick.shrine@4232b74a-9ac4-11dd-a56d-472d35d90729>
2009-07-14 15:14:15 +0000

    logistic plots with coefs attempt

--- Dissertation/derivation.R
+++ Dissertation/derivation.R
@@ -52,3 +52,26 @@ gglog <- function(data, title="", xpos=45, am=1, vjust=0) {
 plotlogistic <- function() {
     gglog(subset(malaria, subset=SUBJID=='54' | SUBJID=='80' | SUBJID=='96' | SUBJID=='98' | SUBJID=='140' | SUBJID=='150' | SUBJID=='176' | SUBJID=='182' | SUBJID=='185' | SUBJID=='187' | SUBJID=='197' | SUBJID=='203'), am=10, vjust=-0.2, title="Logistic fit to log parasite count")
 }
+
+logistic.fit <- function(data) {
+	subjects <- levels(data$SUBJID)
+	n <- length(subjects)
+	fits.df <- data.frame(SUBJID=array(dim=43), SEX=array(dim=43), CENTREID=array(dim=43), trttxt=array(dim=43), A=numeric(n), L=numeric(n), U=numeric(n), B=numeric(n))
+	i = 1
+	for (s in subjects) {
+		fits.df[i, 1:4] <- data[data$SUBJID==s & data$plantm=='PRE-DOSE', c('SUBJID', 'SEX', 'CENTREID', 'trttxt')]
+		fits.df[i, 5:8] <- NA
+		fit <- NULL
+		tryCatch(fit <- nls(log(1 + parct) ~ SSfpl(acttm, A, L, U, B), data=data, subset=SUBJID==s), error=function(e) e)
+		if(!is.null(fit)) {
+			fits.df[i, 5:8] <- coef(fit)
+		}
+		i <- i + 1
+	}
+	return(fits.df)
+}
+
+gglogistic <- function(data, fits, title="", xpos=45, am=1, vjust=0) {
+	q <- gglog90(data, title, xpos, am, vjust)
+	q + geom_hline(data=fits, aes(yintercept=A))
+}
